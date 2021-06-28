c87242b420b409c2c3a1812e5fb32e50d4264953
ryunosuke35 <6bev2103@gmail.com>
2021-06-29 00:59:03 +0900

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'damp-crag-24897.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'limitless-anchorage-55502.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
