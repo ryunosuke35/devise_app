60af2f5964baa481740ca3c11c8519f1fef8d604
ryunosuke35 <6bev2103@gmail.com>
2021-06-30 11:19:28 +0900

    letter_opener

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,12 +96,12 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :letter_opener_web
   #ActionMailer::Base.delivery_method = :smtp
   #ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  #  user_name: ENV['SENDGRID_USERNAME'],
+  #  password: ENV['SENDGRID_PASSWORD'],
+  #  domain: "heroku.com",
+  #  address: "smtp.sendgrid.net",
+  #  port: 587,
+  #  authentication: :plain,
+  #  enable_starttls_auto: true
+  #}
 end
