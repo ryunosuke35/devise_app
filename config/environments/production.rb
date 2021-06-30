f1adc1b5f8039039d2ad39ae82caef36bd231526
ryunosuke35 <6bev2103@gmail.com>
2021-06-30 11:22:55 +0900

    letter_opener

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'dry-brushlands-53780.herokuapp.com' }
   ActionMailer::Base.delivery_method = :letter_opener_web
   #ActionMailer::Base.delivery_method = :smtp
   #ActionMailer::Base.smtp_settings = {
